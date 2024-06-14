aacd2e86ec65ef8ab586cdf869c94382a83f5212
minji0626 <jo010163@naver.com>
2024-06-14 16:54:11 +0900

    spring 6.14

--- ch07SpringDI/src/main/java/kr/spring/ch19/SpringMain.java
+++ ch07SpringDI/src/main/java/kr/spring/ch19/SpringMain.java
@@ -6,7 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringMain {
 	public static void main(String[] args) {
-		// applicationcontext2.xml 설정파일을 읽어들여 스프링 컨테이너를 생성
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("applicationContextScan.xml");
 		//JVM 종료될 때 ApplicationContext를 종료하는 작업을 수행하도록 등록
 		context.registerShutdownHook();
